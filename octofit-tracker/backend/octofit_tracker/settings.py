f1a3affc01940229592e2f3baef46fec9b4a52cb
shantonav <shantonav.sen@gmail.com>
2026-03-27 12:04:17 +0000

    Finalising URLs

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = 'django-insecure-5k*f_oqa5(n7*^pr6xleb5cngi#te2rual^xwik%#2st0)_3lz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
